77df04638eb13e07541093f704b458ac72a9fa83
gdarquie <gaetan.darquie@gmail.com>
2020-01-27 21:25:12 +0100

    Clean git and fetch fragments

--- pages/fragments/index.js
+++ pages/fragments/index.js
@@ -1,23 +1,35 @@
 import Layout from '../../components/Layout';
 import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
 
-const FragmentLink = props => (
-    <li>
-        <Link href="/fragments/[id]" as={`/fragments/${props.title}`}>
-            <a>{props.title}</a>
-        </Link>
-    </li>
+const FragmentCollection = props => (
+    <Layout>
+        <h1>Fragment</h1>
+        <ul>
+            {props.shows.map(fragment => (
+                <li key={fragment.uuid}>
+
+                    <Link href="/fragments/[id]" as={`/fragments/${fragment.title}`}>
+                        <a>{fragment.title}</a>
+                    </Link>
+
+                </li>
+            ))}
+        </ul>
+
+    </Layout>
 );
 
-export default function FragmentCollection() {
-    return (
-        <Layout>
-            <div>
-                <p>Une liste de fragments</p>
-                <ul>
-                    <FragmentLink title="Mon Fragment"/>
-                </ul>
-            </div>
-        </Layout>
-    );
-}
+FragmentCollection.getInitialProps = async function() {
+    // const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+    const res = await fetch('http://127.0.0.1:8000/api/fragments.json');
+    const data = await res.json();
+
+    console.log(`Show data fetched. Count: ${data.length}`);
+
+    return {
+      shows: data.map(entry => entry)
+    };
+};
+
+export default FragmentCollection;
