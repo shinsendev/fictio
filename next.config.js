55f161791f5f36eb251b4c09c493844abe59f83e
gdarquie <gaetan.darquie@gmail.com>
2020-02-24 17:47:13 +0100

    FI-85: clean useless / we can edit a narrative and keep the same uuid

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    env: {
+      edoAPIUrl: 'http://127.0.0.1:8000/api/',
+    },
+  }
---
+++ pages/writing/narratives.js
@@ -0,0 +1,107 @@
+import Layout from '../../components/Layout';
+import uuid from 'uuid';
+import {useState} from 'react';
+
+const WritingNarrative = (props) => {    
+
+    const [count, setCount] = useState(0);
+    const [narrativeUuid, setNarrativeUuid] = useState(getNarrativeUuid(props.narrativeUuidToUpdate));
+
+    function getNarrativeUuid(propsUuid) {
+        // if there is a props, it's an update and we use the narrative uuid send
+        if(propsUuid) {
+            return propsUuid;
+        }
+
+        // else we generate a new uuid
+        return uuid.v4();
+    }
+    
+    function saveAction() {
+        let title = (document.getElementById('narrativeTitle')).value;
+        let content = (document.getElementById('narrativeContent')).value;
+
+        // if user saves many times, she needs to keep the same uuid
+        if (!narrativeUuid) {
+            setNarrativeUuid(getNarrativeUuid(props.narrativeUuidToUpdate, narrativeUuid));
+        }
+        
+        const data = {
+            "uuid": narrativeUuid,
+            "type": "narrative",
+            "title": title,
+            "content": content,
+            "fiction_uuid": '1b7df281-ae2a-40bf-ad6a-ac60409a9ce6'
+        };
+
+        const response = fetch(process.env.edoAPIUrl+'narratives', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data),
+        }).then((response)=> {
+            if (!response.ok) {
+                throw new Error('Error during saving.')
+            }
+            alert(`Text "${narrativeUuid}" has been saved with content "${content}"`);
+            return response.json();
+        });
+    }
+
+    return (
+        <Layout>
+            <div className="container">
+                <div>
+                    <p>Vous avez cliqué {count} fois</p>
+                    <button onClick={() => setCount(count + 1)}>
+                        Cliquez ici
+                    </button>
+                </div>
+                <aside>
+                    <h2>Narratives</h2>
+                    <div>
+                        <button>char</button>
+                    </div>
+                </aside>
+
+                <div className="main">
+                    <div className="tabs">
+                        <span>Tab 1</span>
+                        <span>Tab 2</span>
+                        <span>Tab 3</span>
+                    </div>
+
+                    <div>
+                        <input type="text" id='narrativeTitle'/>
+                        <textarea id='narrativeContent'></textarea>
+                    </div>
+                </div>
+
+                <aside>
+                    <button onClick={saveAction}>save</button>
+                </aside>
+            </div>
+            <style jsx>{`
+                .container {
+                    display:grid;
+                    grid-template-columns: 2fr 8fr 2fr;
+                    border-right: 1px solid grey;
+                    border-bottom: 1px solid grey;
+                }
+
+                textarea {
+                    width:100%;
+                }
+
+                aside {
+                    padding: 3px 12px;
+                }
+
+                h2 {
+                    transform: rotate(-90deg);
+                }
+            `}</style>
+        </Layout>
+    );
+}
+
+export default WritingNarrative;
