84fc7f0cb1713b8150006800c33b3b1a830f9b70
gdarquie <gaetan.darquie@gmail.com>
2020-03-24 12:25:27 +0100

    FI-144: remove comments

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,8 +2,3 @@ module.exports = {
   stories: ['../components/**/*.stories.js'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
 };
-
-// module.exports = {
-//   stories: ['../src/components/**/*.stories.js'],
-//   addons: ['@storybook/preset-create-react-app','@storybook/addon-actions', '@storybook/addon-links'],
-// };
