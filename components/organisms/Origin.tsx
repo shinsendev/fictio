a5ab9370070bd9415947431331b82b529d5d3901
gdarquie <gaetan.darquie@gmail.com>
2020-04-03 16:39:26 +0200

    FI-166: add base for verioning popin

--- components/organisms/Origin.tsx
+++ components/organisms/Origin.tsx
@@ -1,4 +1,5 @@
-import Narrative from '../molecules/Narrative/Narrative'
+import Narrative from '../molecules/Narrative/Narrative';
+import NarrativeVersionedMolecule from '../molecules/NarrativeVersionedMolecule/NarrativeVersionedMolecule';
 import React, { useState } from 'react';
 
 const Origin = props => {
@@ -10,6 +11,10 @@ const Origin = props => {
 
     return (
         <div className="element">
+            <div>
+                <NarrativeVersionedMolecule narrative={props.narratives[0]} />
+            </div>
+
             {props.narratives.map(narrative => 
                 <Narrative isActive={`${(narrative.uuid == activeUuid) ? true : false}`} key={narrative.uuid} narrative={narrative} onClick={() => handleClick(narrative.uuid)}/>
             )}
@@ -17,6 +22,10 @@ const Origin = props => {
                 .element {
                     max-width: 800px;
                     margin: auto;
+                }
+
+                p {
+                    color:white;
                 }
             `}</style>
         </div>
--- pages/about.tsx
+++ pages/about.tsx
@@ -2,16 +2,20 @@ import Layout from '../components/Layout';
 import TextBox from '../components/atoms/TextBox/TextBox';
 import RestoreIcon from '../components/atoms/IconRestore/IconRestore';
 import NarrativeMenu from '../components/molecules/NarrativeMenu/NarrativeMenu';
+import NarrativeVersionedMolecule from '../components/molecules/NarrativeVersionedMolecule/NarrativeVersionedMolecule';
 
-export default function About() {
+const About = props => {
     return (
-        <Layout>
-            <div>
-                <p>This is the about page</p>
-                <TextBox content = "Lorem ipsum"/>
-                <RestoreIcon/>
-                <NarrativeMenu/>
-            </div>
-        </Layout>
+        <div>
+            <NarrativeVersionedMolecule narrative={props.narrative} />
+        </div>
     );
 }
+
+About.getInitialProps = async function (context) {
+    const res = await fetch('http://127.0.0.1:8000/api/narratives/d7d4899d-9b1c-44cd-8406-8c839c16f79f.json');
+    const json = await res.json();
+    return { narrative: json};
+}
+
+export default About;
