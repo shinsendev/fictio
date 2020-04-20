22ef6947e9c5f02e0383659f4e935adf1a11578e
gdarquie <gaetan.darquie@gmail.com>
2020-04-20 21:01:05 +0200

    FI-166: open Versioning modal from origin-template

--- components/templates/modal-versioning-template.tsx
+++ components/templates/modal-versioning-template.tsx
@@ -5,17 +5,19 @@ import React, { useState, Fragment } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
 const ModalVersioningTemplate = props => {
-    const [isOpen, setIsOpen] = useState(props.isOpen);
     const [modalContent, setModalContent] = useState('alternate');
     const [modalVersioning, setModalVersioning] = useState('alternateVersioning');
+
     //const [activeUuid, setActiveUuid,] = useState('uuid');
-//  0 - tests de propagation de le modal 
-//  1 - j' active la modal depuis origin-Template
-//  2 - je fais passer les donnees depuis origin-Template
+    //  0 - tests de propagation de le modal 
+    //  1 - j' active la modal depuis origin-Template
+    //  2 - je fais passer les donnees depuis origin-Template
 
+    // function openModalVersionningTemplate(uuid){
+    //     const myNarrative = getNarrative(uuid);
+    // }
 
-
-function getNarrative() {
+    function getNarrative() {
     fetch('http://127.0.0.1:8000/api/narratives/'+props.activeUuid+'.json')
     .then(response => {
         return response.json();
@@ -25,22 +27,22 @@ function getNarrative() {
             console.log(props.activeUuid);
             setModalVersioning( data.fragments.map(fragment => (<p key={fragment.uuid}>{fragment.content}</p>)));
             });
-} 
+    }
+
     return(    
         <div >
-            <h1> Mon titre est : {props.titre} </h1>
-            <h2> Mon uuid est : {props.activeUuid} </h2>  
+            <h2> Mon uuid est : {props.narrativeUuid} </h2>  
             <div >
                 <p>
                     {modalContent}
                 </p>
             </div>
             <Modal
-                    open={isOpen}
+                    open={props.isOpen}
                     aria-labelledby="simple-modal-title"
                     aria-describedby="simple-modal-description">
                     <div> 
-                        <div><p>texte</p></div>
+                        <p>{modalContent}</p>
                     </div>
             </Modal>
             <style global jsx>{`
--- components/templates/origin-template.tsx
+++ components/templates/origin-template.tsx
@@ -10,8 +10,9 @@ const OriginTemplate = props => {
     const [isOpen, setIsOpen] = useState(false);
     const [modalContent, setModalContent] = useState('alternate');
     const [modalVersioning, setModalVersioning] = useState('alternateVersioning');
-    const [activeUuid, setActiveUuid,] = useState('uuid');
-   
+    const [activeUuid, setActiveUuid] = useState('uuid');
+    const [activeNarrative, setActiveNarrative] = useState(null);
+
     const useStyles = makeStyles((theme) => ({
         versioning: {
           position: 'absolute',
@@ -28,105 +29,127 @@ const OriginTemplate = props => {
       }));
 
 
-    const classes = useStyles();
+    // const classes = useStyles();
 
-    const content = (
-        <div className={classes.versioning} >
-            <div className="cross" onClick={ToggleSatusModal}>
-                <CrossDelete />
-            </div>
-            <article>
-                <div>
-                    <p>
-                        {modalContent}
-                    </p>
-                    <button onClick={handleSaveAndClose}>SAVE AND CLOSE</button>
-                </div>
-                <div>
-                    {modalVersioning}
-                </div>
-            </article>
-            <style global jsx>{`
-            .cross {
-                position:absolute;
-                top:20px;
-                right:20px;
-            }
-                article {
-                    width:100%;
-                    display:flex;
-                    flex-direction:row;
-                    justify-content:center;
-                    padding-top:20px;
-                }
-                div {
-                    margin: 0px 10px 0px 10px ;
-                    padding:0 5% 0 5% 0 5% 0 5%;
-                    flex-direction:column;
-                }
-                p {
-                    padding: 10px 20px;
-                    background: #262626;
-                    color: white;
-                    max-width: 650px;
-                    border-radius: 5px;
-                    min-height: 45px;
-                    /* max-height: 150px; */
-                    margin-bottom : 10px;
-                }
-                button {
-                    display:block;
-                    margin:auto;
-                    background: black;
-                    color:white;
-                    border:1px solid white;
-                    border-radius: 5px;
-                    text-align:center;
-                    padding:10px 10px 10px 10px;
-                }
-            `}</style>
-        </div>
-       
-    );
-     function ToggleSatusModal() {
+    // const content = (
+    //     <div className={classes.versioning} >
+    //         <div className="cross" onClick={ToggleSatusModal}>
+    //             <CrossDelete />
+    //         </div>
+    //         <article>
+    //             <div>
+    //                 <p>
+    //                     {modalContent}
+    //                 </p>
+    //                 <button onClick={handleSaveAndClose}>SAVE AND CLOSE</button>
+    //             </div>
+    //             <div>
+    //                 {modalVersioning}
+    //             </div>
+    //         </article>
+    //         <style global jsx>{`
+    //         .cross {
+    //             position:absolute;
+    //             top:20px;
+    //             right:20px;
+    //         }
+    //             article {
+    //                 width:100%;
+    //                 display:flex;
+    //                 flex-direction:row;
+    //                 justify-content:center;
+    //                 padding-top:20px;
+    //             }
+    //             div {
+    //                 margin: 0px 10px 0px 10px ;
+    //                 padding:0 5% 0 5% 0 5% 0 5%;
+    //                 flex-direction:column;
+    //             }
+    //             p {
+    //                 padding: 10px 20px;
+    //                 background: #262626;
+    //                 color: white;
+    //                 max-width: 650px;
+    //                 border-radius: 5px;
+    //                 min-height: 45px;
+    //                 /* max-height: 150px; */
+    //                 margin-bottom : 10px;
+    //             }
+    //             button {
+    //                 display:block;
+    //                 margin:auto;
+    //                 background: black;
+    //                 color:white;
+    //                 border:1px solid white;
+    //                 border-radius: 5px;
+    //                 text-align:center;
+    //                 padding:10px 10px 10px 10px;
+    //             }
+    //         `}</style>
+    //     </div>
+    // );
+
+    //  function ToggleSatusModal() {
+    //     if (isOpen === true) {
+    //         setIsOpen(false);
+    //     } else {
+    //         setIsOpen(true);
+    //     }
+    //   } 
+
+    // function handleSaveAndClose() {
+    //     alert('save');
+    //     ToggleSatusModal();
         
-        if (isOpen === true) {
-            setIsOpen(false);
-        } else {
-            setIsOpen(true);
-        }
-      } 
+    // }
 
-    function handleSaveAndClose() {
-        alert('save');
-        ToggleSatusModal();
-        
-    }
-    function openModalOriginsTemplate(uuid){
-        const myNarrative = getNarrative(uuid);
-        setActiveUuid(uuid);
-    }
-
-    // my fetch narrative function
-      function getNarrative(uuid) {
-        fetch('http://127.0.0.1:8000/api/narratives/'+uuid+'.json')
+    function openModalOriginTemplate(uuid){
+        fetch(process.env.edoAPIUrl + 'narratives/'+uuid+'.json')
         .then(response => {
             return response.json();
-            }).then(data => {
-                setIsOpen(true);
+        })
+        .then(data => {
                 setModalContent(data.content);
                 setModalVersioning( data.fragments.map(fragment => (<p key={fragment.uuid}>{fragment.content}</p>)));
-                });
-    }   
+                setActiveNarrative(data);
+                setActiveUuid(uuid);
+        })
+        .then(data => {
+            setIsOpen(true);
+        });
+    }
+
+    // // my fetch narrative function
+    //   function getNarrative(uuid) {
+    //     fetch(process.env.edoAPIUrl + 'narratives/'+uuid+'.json')
+    //     .then(response => {
+    //         console.log('mais nonnn');
+    //         return response.json();
+    //     })
+    //     .then(data => {
+    //             setModalContent(data.content);
+    //             setModalVersioning( data.fragments.map(fragment => (<p key={fragment.uuid}>{fragment.content}</p>)));
+    //             setActiveNarrative(data)
+    //     })
+    //     .then(data => {
+    //         setIsOpen(true);
+    //     });
+    // }
  
     return (
         <div>
             <Header />
-            <div className="container">              
-                <Origin 
+            <div className="container">
+                <ModalVersioningTemplate 
+                    isOpen={isOpen}
+                    narrativeUuid={activeUuid}
+                    content={modalContent}
+                    narrative={activeNarrative}
+                />
+                <Origin
                     narratives={props.narratives} 
-                    openModal={openModalOriginsTemplate} 
-                     />
+                    openModal={openModalOriginTemplate}
+                />
             </div>
             <style jsx>{`
                 .container {
