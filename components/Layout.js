1ff5551db2e4c89312d34f1798a6e4f2ba32cd7c
gdarquie <gaetan.darquie@gmail.com>
2020-05-05 20:46:01 +0200

    FI-176: clean useless files

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,10 @@
-import Header from './Header';
 import Footer from './Footer'
 
 const layoutStyle = {};
 
 const Layout = props => (
     <div style={layoutStyle}>
-        <Header />
         {props.children}
-        <Footer/>
     </div>
 );
 
