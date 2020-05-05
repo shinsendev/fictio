01d61329cebde5eda2e14171170cdfa7debfd7d6
gdarquie <gaetan.darquie@gmail.com>
2020-05-05 13:00:08 +0200

    FI-167_V2: clean code

--- components/molecules/NarrativeMolecule/NarrativeMolecule.js
+++ components/molecules/NarrativeMolecule/NarrativeMolecule.js
@@ -7,7 +7,6 @@ import { Draggable } from 'react-beautiful-dnd';
 
 const Narrative = props => {
     const [narrativeState, setNarrativeState] = useState(props.narrative);
-    const [displayArrowState, setDisplayArrowState] = useState('down');
 
     function openModalNarrative() {
         props.openModal(narrativeState.uuid);
@@ -49,13 +48,6 @@ const Narrative = props => {
     }
 
     function handleDisplay() {
-        if (displayArrowState === 'down') {
-            setDisplayArrowState('up');
-        }
-        else {
-            setDisplayArrowState('down');
-        }
-
         props.hideNarrativeChildren(narrativeState);
     }
 
